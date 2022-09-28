779f2da3fe0c7ff3747788779f57ac8cdbfaa955
Yasir284 <lambawalay@gmail.com>
2022-09-28 23:07:53 +0530

    almost completed the project

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-let min = document.querySelector(".min");
-let sec = document.querySelector(".sec");
-let milisec = document.querySelector(".miliSec");
+let counter = document.querySelector(".counter");
+let [miliSec, sec, min, hour] = [0, 0, 0, 0];
+let int = null;
 
-function start() {}
-function pause() {}
-function reset() {}
+function start() {
+  if (int !== null) {
+    clearInterval(int);
+  }
+
+  int = setInterval(getCountDown, 10);
+}
+
+function stop() {
+  clearInterval(int);
+}
+function reset() {
+  clearInterval(int);
+  [miliSec, sec, min, hour] = [0, 0, 0, 0];
+  counter.textContent = "00 : 00 : 00 : 000";
+}
+
+function getCountDown() {
+  miliSec += 10;
+
+  if (miliSec == 1000) {
+    miliSec = 0;
+    sec++;
+  }
+  if (sec == 60) {
+    sec = 0;
+    min++;
+  }
+  if (min == 60) {
+    min = 0;
+    hour++;
+  }
+
+  let h = hour < 10 ? "0" + hour : hour;
+  let m = min < 10 ? "0" + min : min;
+  let s = sec < 10 ? "0" + sec : sec;
+  let ms =
+    miliSec < 10 ? "00" + miliSec : miliSec < 100 ? "0" + miliSec : miliSec;
+  counter.textContent = `${h} : ${m} : ${s} : ${ms}`;
+}
