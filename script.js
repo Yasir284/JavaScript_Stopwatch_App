d1e644867b5fc1e6116bff9200f52faceee6074b
Yasir284 <lambawalay@gmail.com>
2022-09-28 19:13:03 +0530

    initial push

---
+++ script.js
@@ -0,0 +1,7 @@
+let min = document.querySelector(".min");
+let sec = document.querySelector(".sec");
+let milisec = document.querySelector(".miliSec");
+
+function start() {}
+function pause() {}
+function reset() {}
